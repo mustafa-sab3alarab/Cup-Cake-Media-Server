470f9fd84b30891efb4fa6df29aa6d9416438a6b
ameeramjed <pr.ameer98@gmail.com>
2023-06-01 14:34:45 +0300

    clean up delete functions create and delete job title

--- src/main/kotlin/com/the_chance/data/jobTitle/JobTitleService.kt
+++ src/main/kotlin/com/the_chance/data/jobTitle/JobTitleService.kt
@@ -3,7 +3,6 @@ package com.the_chance.data.jobTitle
 
 import com.the_chance.data.utils.dbQuery
 import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 
@@ -15,46 +14,6 @@ class JobTitleService(private val database: Database) {
         }
     }
 
-
-    suspend fun createJobTitle(newTitle: String): Pair<JobTitle, Boolean> = dbQuery {
-        try {
-            JobTitleTable.insertIgnore { it[title] = newTitle }.let { newJobTitle ->
-                JobTitle(
-                    newJobTitle[JobTitleTable.id].value,
-                    newJobTitle[JobTitleTable.title]
-                )
-            }.let { Pair(it, true) }
-        } catch (e: Exception) {
-            Pair(
-                findJobTitleByTitle(newTitle) ?: throw IllegalStateException("Failed to create job title"),
-                false
-            )
-        }
-    }
-
-    //region search
-    private suspend fun findJobTitleByTitle(title: String): JobTitle? {
-        return searchJobTitle { JobTitleTable.title eq title }
-    }
-    suspend fun findJobTitleById(id: Int): JobTitle? {
-        return searchJobTitle { JobTitleTable.id eq id }
-    }
-
-    private suspend fun searchJobTitle(where: SqlExpressionBuilder.() -> Op<Boolean>): JobTitle? {
-        return dbQuery {
-            JobTitleTable.select(where).singleOrNull()
-                ?.let { jobTitle ->
-                    JobTitle(
-                        jobTitle[JobTitleTable.id].value,
-                        jobTitle[JobTitleTable.title],
-                    )
-                }
-
-        }
-    }
-
-    //endregion
-
    suspend fun getAllJobTitle():List<JobTitle> = dbQuery {
        JobTitleTable.selectAll().map {jobTitle ->
            JobTitle(
@@ -65,8 +24,11 @@ class JobTitleService(private val database: Database) {
        }
    }
 
-    suspend fun deleteJobTitleById(id: Int): Boolean = dbQuery {
-        JobTitleTable.deleteWhere { JobTitleTable.id eq id } > 0
+    suspend fun isJobTitleIdValid(id: Int): Boolean {
+        return dbQuery {
+            JobTitleTable.select { JobTitleTable.id eq id }.empty().not()
+        }
     }
+
 
 }
--- src/main/kotlin/com/the_chance/endpoints/JobTitle.kt
+++ src/main/kotlin/com/the_chance/endpoints/JobTitle.kt
@@ -1,39 +1,14 @@
 package com.the_chance.endpoints
 
-import com.the_chance.data.jobTitle.JobTitle
 import com.the_chance.data.jobTitle.JobTitleService
 import com.the_chance.data.utils.ServerResponse
-import com.the_chance.utils.isInt
-import com.the_chance.utils.isValidUUID
-import com.the_chance.utils.validationTitle
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Routing.jobTitleRoute(jobTitleService: JobTitleService) {
 
-
-    post("/jobTitle") {
-
-        suspend fun createJobTitle(title: String) = jobTitleService.createJobTitle(title)
-
-        suspend fun handleJobTitleResult(result: Pair<JobTitle, Boolean>) {
-            result.takeIf { it.second }?.let { jobTitleCreated ->
-                call.respond(HttpStatusCode.Created, ServerResponse.success(jobTitleCreated.first))
-            } ?: call.respond(HttpStatusCode.Conflict, ServerResponse.success(result.first))
-        }
-
-        call.receiveParameters()["jobTitle"]?.let { title ->
-            title.takeIf { validationTitle(it) == null }?.let {
-                handleJobTitleResult(createJobTitle(title))
-            } ?: call.respond(HttpStatusCode.BadRequest, ServerResponse.error(
-                    validationTitle(title) ?: "Failed to create job title")
-            )
-        } ?: call.respond(HttpStatusCode.BadRequest, ServerResponse.error("Title is required"))
-
-    }
 
     get("/jobTitles") {
         jobTitleService.getAllJobTitle().takeIf { it.isNotEmpty() }?.let { posts ->
@@ -41,23 +16,6 @@ fun Routing.jobTitleRoute(jobTitleService: JobTitleService) {
         } ?: call.respond(HttpStatusCode.NoContent)
     }
 
-    delete("/jobTitle/{idJobTitle}") {
-
-        suspend fun deleteJobTitle(idJobTitle: Int) {
-            jobTitleService.deleteJobTitleById(idJobTitle).takeIf { it }?.let { result ->
-                call.respond(HttpStatusCode.OK, ServerResponse.success(result))
-            } ?: call.respond(HttpStatusCode.NotFound, ServerResponse.error("Opps!, this job title not found."))
-        }
-
-        call.parameters["idJobTitle"]?.let { id ->
-            id.takeIf {
-                it.isInt()
-            }?.let { idJobTitle ->
-                deleteJobTitle(idJobTitle.toInt())
-            } ?: call.respond(HttpStatusCode.BadRequest)
-        }
-
-    }
 
 }
 
